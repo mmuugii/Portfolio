48822bae775778c6051513e14ba832177de14a3c
Muugii Munkhbold <mmunkhbold94@gmail.com>
2023-01-17 23:21:08 -0800

    updated sections

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
-// import { testimonials } from "../data";
 
 export default function Resume() {
   return (
